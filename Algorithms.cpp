601fdb9ad34cdb4d89df4eeee122f40566c591d9
Vladislav <vladik.rjazancevs@gmail.com>
2023-11-05 15:35:20 +0200

    queue functions added

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
 #include<cstdlib>
 #include<conio.h>
-#define N 100
 
 using namespace std;
 
 struct Node
 {
-  int data; // 4 bytes
-  Node *link; // 4 bytes for adress as it is integer
+  int data;
+  Node *link;
 };
 
 void ProgEnd()
@@ -18,9 +17,9 @@ void ProgEnd()
   cout <<"              ,---------------------------," << endl;
   cout <<"              |  ---------------------  |"<< endl;
   cout <<"              | |                       | |"<< endl;
-  cout <<"              | |     Linked            | |"<< endl;
-  cout <<"              | |         List          | |"<< endl;
-  cout <<"              | |            Program    | |"<< endl;
+  cout <<"              | |      Queue            | |"<< endl;
+  cout <<"              | |          Program      | |"<< endl;
+  cout <<"              | |                       | |"<< endl;
   cout <<"              | |                       | |"<< endl;
   cout <<"              |  _____________________  |"<< endl;
   cout <<"              |___________________________|"<< endl;
@@ -39,441 +38,250 @@ void ProgEnd()
 Node *CreateNode()
 {
   Node *newNode;
-  newNode = new Node;	// 8 bytes for new node
+  newNode = new Node;
   cout << "\nEnter number to node: ";
   cin >> newNode->data;
   newNode->link = NULL;
   return newNode;
 }
 
-void PrintList (Node *head)
+class Queue
 {
-  Node *cur; // adress
-  cout<<"\nList elements:\n";
-  cur = head;
-  while (cur != NULL)
+  private:
+      Node *back; // last element (right site)
+      Node *front; // first element (left site)
+      int size;
+
+  public:
+      void Init();
+      void Enqueue(); // add node queue at the end
+      void Dequeue(); // remove node at the beginning
+      void PrintQueue();
+      int Size();
+      void RemoveAll();
+};
+
+void Queue::Init()
+{
+  back = NULL;
+  front = NULL;
+  size = 0;
+}
+
+void Queue::Enqueue()
+{
+  Node *newNode;
+  newNode = CreateNode();
+
+  if(back == NULL) // if queue is empty
+  {
+    // first node is front & back
+    back = newNode;
+    front = newNode;
+  }
+  else // if queue is not empty
+  {
+    back->link = newNode;
+    back = newNode;
+  }
+  size++;
+}
+
+void Queue::Dequeue()
+{
+  if(front == NULL) // if queue is empty
+  {
+    cout << "Queue is empty! Nothing to remove!\n";
+  }
+  else // if queue is not empty we can remove
+  {
+    if(front == back) // if there 1 node
     {
-      cout << cur->data << " ";
-      cur = cur->link; //pareja uz nakamo mezgli
-    }
-}
-
-Node *AddFirst(Node *head) // return adress
-{
-  Node *cur;
-  cur = new Node; // izdalam ram uz 8 bait
-  cur = CreateNode();
-  cur->link = head;
-  head = cur;
-  return head;
-}
-
-Node *AddEnd(Node *head)
-{
-  Node *cur, *cur1;
-  // new node -----------
-  cur1 = new Node;
-  cur1 = CreateNode();
-  // --------------------
-
-  // list travelling
-  cur = head;
-  while (cur->link != NULL)
-  {
-      cur = cur->link;
-  }
-  cur->link = cur1;
-
-  return head;
-}
-
-Node *FindNode(Node *head, int x)
-{
-  Node *cur;
-  cur = head;
-
-  while(cur != NULL && cur->data != x)
-  {
-      cur = cur->link;
-  }
-
-  return cur;
-}
-
-Node *PreviousNode(Node *head, int x)
-{
-  Node *cur, *prev;
-  cur = head;
-
-  while(cur != NULL && cur->data != x)
-  {
-      prev = cur;
-      cur = cur->link;
-  }
-
-  return prev;
-}
-
-Node *AddBefore(Node *head)
-{
-  // ask user
-  int x;
-  Node *cur, *newNode, *prev;
-  cout << "Before which node add: ";
-  cin >> x;
-
-  // exist or not exist
-
-  cur = FindNode(head, x);
-  if(cur)
-  {
-    cout << "Node exist!\n";
-    newNode = CreateNode();
-    prev = PreviousNode(head, x);
-    if(head->data == x)
-    {
-      newNode->link = head;
-      head = newNode;
+      Node *temp = front;
+      front = front->link;
+      delete temp;
+      size--;
+      back = NULL;
     }
     else
     {
-      prev->link = newNode;
-      newNode->link = cur;
-      //newNode->link = cur;
-      //prev->link = newNode;
+      Node *temp = front;
+      front = front->link;
+      delete temp;
+      size--;
     }
-    cout << "New node is added!\n";
+    cout << "Node has been removed!" << endl;
   }
-  else cout << "Node does not exist!\n";
-
- return head;
 }
 
-Node *NextNode(Node *head, int x)
+void Queue::PrintQueue()
 {
-  Node *cur, *next;
-  cur = head;
-
-  while(cur != NULL && cur->data != x)
+  if(front == nullptr) // if queue is empty
   {
-      cur = cur->link;
+    cout << "Queue is empty!\n";
   }
-  //cur = cur->link;
-  next = cur->link;
-
-  return next;
-}
-
-Node *AddAfter(Node *head)
-{
-  int x;
-  Node *cur, *newNode, *next;
-  cout << "After which node add: ";
-  cin >> x;
-
-  cur = FindNode(head, x);
-
-  if(cur)
+  else // if queue is not empty
   {
-    cout << "Node exist!\n";
-    newNode = CreateNode();
-    next = NextNode(head, x);
-
-    if(!next)
+    Node *cur = front;
+    cout << "Queue elements:\n\n";
+    while (cur != nullptr)
     {
-      cur->link = newNode;
-    }
-    else
-    {
-      cur->link = newNode;
-      newNode->link = next;
-    }
-
-    cout << "New node is added!\n";
-  }
-  else cout << "Node does not exist!\n";
-
-  return head;
-}
-
-int NumberOfNodes(Node *head)
-{
-  Node *cur;
-  cur = head;
-  int amount = 0;
-
-  while(cur != NULL)
-  {
-    cur = cur->link;
-    amount++;
-  }
-
-  return amount;
-}
-
-Node* RemoveNodeBeginning(Node *head)
-{
-  Node* cur;
-  cur = head->link; // next node after head
-  delete head;
-
-  return cur; // return new head
-}
-
-Node* RemoveNodeEnd(Node *head)
-{
-  Node *cur, *prev;
-
-  if(head->link != NULL) // if there is more nodes after head
-  {
-    cur = head;
-
-    while(cur->link!=NULL)
-    {
-      prev = cur;
+      cout << " " << cur->data;
       cur = cur->link;
     }
-    delete cur; // delete last
-    prev->link = NULL;
   }
-  else // if head is the one node
-  {
-    head = RemoveNodeBeginning(head);
-  }
-  return head;
 }
 
-Node* RemoveAll(Node *head)
+int Queue::Size()
 {
-  Node* cur;
-  while(head != nullptr)
-  {
-    cur = head;
-    head = head->link; // next node
-    delete cur; // delete current node
-  }
-  //head = nullptr;
-  return head;
+    return size;
 }
 
-Node* RemoveBefore(Node *head)
+void Queue::RemoveAll()
 {
-  int x;
-  cout << "Before which node remove: ";
-  cin >> x;
-
-  Node *cur, *prev, *prev2;
-
-  cur = FindNode(head, x);
-
-  if(cur)
+  if(front == NULL)
   {
-    cout << "Node exist!\n";
-    if(head->data == x)
-    {
-      cout << "Nothing to delete!\n";
-    }
-    else
-    {
-      prev = PreviousNode(head, x);
-      if(prev->data == head->data)
-      {
-        head = cur;
-        delete prev;
-      }
-      else
-      {
-        prev2 = PreviousNode(head, prev->data);
-        prev2->link = cur;
-        delete prev;
-      }
-      cout << "Node has been removed!\n";
-    }
+    cout << "Queue is empty! Nothing to remove!\n";
   }
-  else cout << "Node does not exist!\n";
-  return head;
-}
-
-Node* RemoveAfter(Node *head)
-{
-  int x;
-  cout << "After which node remove: ";
-  cin >> x;
-
-  Node *cur, *next, *next2;
-  cur = FindNode(head, x);
-
-  if(cur)
+  else
   {
-    cout << "Node exist!\n";
-    if(cur->link == NULL)
+    Node *temp;
+    while (front != nullptr)
     {
-      cout << "Nothing to delete!\n";
+        temp = front;
+        front = front->link;
+        delete temp;
     }
-    else
-    {
-      next = NextNode(head, x);
-      if(next->link == NULL)
-      {
-        head = RemoveNodeEnd(head);
-      }
-      else
-      {
-        next2 = NextNode(head, next->data);
-        delete next;
-        cur->link = next2;
-      }
-      cout << "Node has been removed!\n";
-    }
+    size = 0;
+    back = NULL;
+    cout << "All nodes have been removed!\n";
   }
-  else cout << "Node does not exist!\n";
-  return head;
 }
 
 int main()
 {
-  Node *head = NULL; // create place in computer memory adress
-  // node is not created
-  enum KEYS { num1 = 49, num2 = 50, num3 = 51 , num4 = 52, num5 = 53, num6 = 54, num7 = 55, num8 = 56, num9 = 57, num10 = 45, num11 = 61, num12 = 112, num13 = 91, num14 = 93 };
-  /*
-  num10 = 45; it is   -
-  num11 = 61; it is   =
-  num12 = 112; it is  p
-  num13 = 91; it is   [
-  num14 = 93; it is   ]
-  */
+  Queue q;
+
+  enum KEYS { num1 = 49, num2 = 50, num3 = 51 , num4 = 52, num5 = 53, num6 = 54, num7 = 55 };
+
   int choice;
+  int limit = 0;
 
   system("cls");
-  cout << "\n\t\tLinked List\n\n";
-  cout << "\n Author: Vladislav Ryazancev\n Ver: 5.2.7\n Date (start): 06.09.2023 / 14:33\n Date (end): 11.10.2023 / 16:13\n\n";
+  cout << "\n\t\tQueue\n\n";
+  cout << "\n Author: Vladislav Ryazancev\n Ver: 2.1.1\n Date (start): 05.11.2023 / 13:31\n Date (end): 05.11.2023 / 16:13\n\n";
 
   do
     {
       system("pause>nul");
       system("cls");
 
-      cout << "\n What do you want to do: \n\n";
-      cout << "1. Create head\n";
-      cout << "2. Print head value and link\n";
-      cout << "3. Add node to list beginning\n";
-      cout << "4. Add node to list end\n";
-      cout << "5. Add node before\n";
-      cout << "6. Add node after\n";
-      cout << "7. Show how many nodes exist\n";
-      cout << "8. Print List\n";
-      cout << "9. Remove node at the beginning\n";
-      cout << "10. Remove node at the end (-)\n";
-      cout << "11. Remove node before (=)\n";
-      cout << "12. Remove node after (p)\n";
-      cout << "13. Remove all nodes ([)\n";
-      cout << "14. Stop Program (])\n\n";
+      cout << "\n\t\t -- MENU --\n\n";
+      cout << "-------------- INITIALIZATION ----------------\n";
+      cout << "1. Initialize queue\n";
+      cout << "\n------------------- ADD ----------------------\n";
+      cout << "2. Add node at the queue end\n";
+      cout << "\n------------------ REMOVE --------------------\n";
+      cout << "3. Remove node at the queue beginning\n";
+      cout << "4. Remove all nodes\n";
+      cout << "\n-------------- PRINT & COUNT -----------------\n";
+      cout << "5. Print queue\n";
+      cout << "6. Show how many nodes exist\n";
+      cout << "\n------------------ STOP ----------------------\n";
+      cout << "7. Stop Program\n";
+      cout << "\n----------------------------------------------\n\n";
 
       choice = getch();
 
       switch(choice)
       {
-        case num1: head = CreateNode(); break;
+        case num1:
+        {
+          if(limit == 0)
+          {
+            q.Init();
+            cout << "Queue was initialized!" << endl;
+            limit++;
+          }
+          else
+          {
+            cout << "ERROR!\nQueue is already initialized!" << "\n\n";
+          }
+          break;
+        }
         case num2:
         {
-          if(head)
+          if(limit == 0)
           {
-            cout << "Head data: " << head->data << endl;
-            cout << "Head link: " << head->link << endl;
+            cout << "Can NOT add! Queue is not initialized!\n";
           }
-          else cout << "Head does not exists! Create Head!\n";
-          system("pause>nul");
+          else
+          {
+            q.Enqueue();
+          }
           break;
         }
-        case num3: // add node to list beginning
-                head = AddFirst(head);
-                break;
-        case num4: // add node to list end
+        case num3:
         {
-          if(head)
+          if(limit == 0)
           {
-            AddEnd(head);
+            cout << "Can NOT remove! Queue is not initialized!\n";
           }
-          else cout << "Head does not exists! Create Head!\n";
+          else
+          {
+            q.Dequeue();
+          }
           break;
         }
-        case num5: // add node before specific node
+        case num4:
         {
-          if(head)
+          if(limit == 0)
           {
-            head = AddBefore(head);
+            cout << "Can NOT remove! Queue is not initialized!\n";
           }
-          else cout << "Head does not exists! Create Head!\n";
+          else
+          {
+            q.RemoveAll();
+          }
+          break;
+        }
+        case num5:
+        {
+          if(limit == 0)
+          {
+            cout << "Can NOT print! Queue is not initialized!\n";
+          }
+          else
+          {
+            q.PrintQueue();
+          }
           break;
         }
         case num6:
         {
-          if(head)
+          if(limit == 0)
           {
-            AddAfter(head);
+            cout << "Can NOT print! Queue is not initialized!\n";
           }
-          else cout << "Head does not exists! Create Head!\n";
+          else if(q.Size() == 0)
+          {
+            cout << "Nodes count: 0\n";
+          }
+          else
+          {
+            cout << "Nodes count: " << q.Size() << endl;
+          }
           break;
         }
         case num7:
         {
-          int amount = NumberOfNodes(head);
-          cout << "Now there is " << amount << " node/s!";
-          break;
-        }
-        case num8: PrintList(head); break;
-        case num14: cout << "\nProgram is stopped! Goodbye!"; ProgEnd(); break;
-        case num9:
-        {
-          if(head)
-          {
-            head = RemoveNodeBeginning(head);
-            cout << "First node has been removed!\n";
-          }
-          else cout << "Head does not exists! Create Head!\n";
-          break;
-        }
-        case num10:
-        {
-          if(head)
-          {
-            head = RemoveNodeEnd(head);
-            cout << "Last node has been removed!\n";
-          }
-          else cout << "List is empty! Can not remove!\n";
-          break;
-        }
-        case num11:
-        {
-          if(head)
-          {
-            head = RemoveBefore(head);
-          }
-          else cout << "List is empty! Can not remove!\n";
-          break;
-        }
-        case num12:
-        {
-          if(head)
-          {
-            head = RemoveAfter(head);
-          }
-          else cout << "List is empty! Can not remove!\n";
-          break;
-        }
-        case num13:
-        {
-          if(head)
-          {
-            head = RemoveAll(head);
-            cout << "All nodes have been removed!!\n";
-          }
-          else cout << "List is empty! Can not remove!\n";
+          cout << "\nProgram is stopped! Goodbye!";
+          ProgEnd();
           break;
         }
         default: cout << "Incorrect input!\n";
       }
-    } while(choice != num14);
+    } while(choice != num7);
 
   system("pause>nul");
   return 0;
